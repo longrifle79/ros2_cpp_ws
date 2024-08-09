cfb7dc8425af5327c4381ada50ebe1428c1b054a
longrifle79 <studlee752@yahoo.com>
2024-08-09 16:48:29 -0700

    update cpp

--- Workspaces/ros2_cpp_ws/src/udemy_ros2_pkg/src/publisher.cpp
+++ Workspaces/ros2_cpp_ws/src/udemy_ros2_pkg/src/publisher.cpp
@@ -1,9 +1,15 @@
 #include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
 
 class HelloWorldPubNode : public rclcpp::Node
 {
     public:
-        HelloWorldPubNode() : Node("hello_world_pub_node") {}
+        HelloWorldPubNode() : Node("hello_world_pub_node") 
+        {
+            publisher_ = this->create_publisher<std_msgs::msg::String("Hello world", 10);
+        }
+        private:
+            rclpp::Publisher<std_msgs::msg::String>::SharedPtr_;
 };
 
 
