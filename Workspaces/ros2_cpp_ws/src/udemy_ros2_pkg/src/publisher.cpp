291b3e78513d33276d45be7afeeb38c46e6684f5
longrifle79 <studlee752@yahoo.com>
2024-08-09 16:23:37 -0700

    fixed cpp file

--- Workspaces/ros2_cpp_ws/src/udemy_ros2_pkg/src/publisher.cpp
+++ Workspaces/ros2_cpp_ws/src/udemy_ros2_pkg/src/publisher.cpp
@@ -1,2 +1,13 @@
 #include "rclcpp/rclcpp.hpp"
 
+class HelloWorldPubNode : public rclcpp::Node
+{
+    public:
+        HelloWorldPubNode() : Node("hello_world_pub_node") {}
+};
+
+
+int main()
+{
+    return 0;
+}
